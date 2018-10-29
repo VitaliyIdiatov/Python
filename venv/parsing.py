74da2e51dedf075d9886ce938accf2d72017e87a
Vitaliy Idiatov <iadernik@gmail.com>
2018-10-29 22:31:50 +0500

    All tables parsed to csv

--- venv/parsing.py
+++ venv/parsing.py
@@ -18,12 +18,24 @@ r_str = re.sub(r'  ', ' ', r_str)
 with open('test.html', 'w') as output_file:
     output_file.write(r_str)
 
+# parsing tables' titles
+regex = '(<h3>)(.*?)(</h3>)'
+r_pattern = re.compile(regex)
+r_titles = re.findall(r_pattern, r_str)
+print(r_titles[17][1])
+
+# parsing tables' heads
 regex = '(<thead><tr><th>)(.*?)(</th></tr></thead>)'
 r_pattern = re.compile(regex)
 r_heads = re.findall(r_pattern, r_str)
 
+# parsing tables' data
+regex = '(<tbody><tr><td.*?>)(.*?)(</td></tr></tbody>)'
+r_pattern = re.compile(regex)
+r_data = re.findall(r_pattern, r_str)
+
 csv = open('parsed.csv', 'w')
 
 for x in range(len(r_heads)):
-    row = re.sub(r'</th><th>', ', ', r_heads[x][1]) + '\n'
-    csv.write(row)  # 17 items inside list
+    head_row = r_titles[x][1] + '\n' + re.sub(r'</th><th>', ', ', r_heads[x][1]) + '\n' + re.sub(r'</td></tr><tr><td.*?>', '\n', re.sub(r'</td><td.*?>', ', ', r_data[x][1])) + '\n'
+    csv.write(head_row)
